4d62bad594645e2a4f351b3554a0e19d7bfede8e
Borja <btiradog@alumnos.unex.es>
2021-04-12 11:16:43 +0200

    rm

--- components/drone_controller/src/CMakeFiles/ICES__home_borja_drone_controller_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_borja_drone_controller_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
